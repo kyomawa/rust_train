f209d1499fa04d66404e0c0db82c47444b8cbe07
kyomawa <kyomawa@protonmail.com>
2025-01-16 16:34:17 +0100

    feat: list_taks fn added

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{ collections::HashMap, io };
 
 #[derive(Debug)]
 enum TaskStatus {
@@ -20,7 +20,27 @@ impl Task for String {
     }
 }
 
+fn add_task<T: Task>(
+    tasks: &mut HashMap<i32, (T, TaskStatus)>,
+    description: T,
+    next_task_id: &mut i32
+) {
+    tasks.insert(*next_task_id, (description, TaskStatus::Incomplete));
+    *next_task_id += 1;
+    println!("Tâche ajoutée !");
+}
+
+fn list_tasks<T: Task>(tasks: &HashMap<i32, (T, TaskStatus)>) {
+    println!("Liste des tâches :");
+    for (id, (description, status)) in tasks {
+        println!("{}: {}, Status: {:?}", id, description.description(), status);
+    }
+}
+
 fn main() {
+    let mut tasks: HashMap<i32, (String, TaskStatus)> = HashMap::new();
+    let mut next_task_id = 1;
+
     loop {
         println!("Gestionnaire de liste de tâches");
         println!("1. Ajouter une tâche");
@@ -33,6 +53,15 @@ fn main() {
         io::stdin().read_line(&mut choice).expect("Impossible de lire la ligne.");
 
         match choice.trim() {
+            "1" => {
+                println!("Entrez une description.");
+                let mut description = String::new();
+                io::stdin().read_line(&mut description).expect("Impossible de lire la ligne.");
+                add_task(&mut tasks, description.trim().to_string(), &mut next_task_id);
+            }
+            "2" => {
+                list_tasks(&tasks);
+            }
             "5" => {
                 println!("Gestionnaire fermé.");
                 break;
